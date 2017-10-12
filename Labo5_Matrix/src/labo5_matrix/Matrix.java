01d73f30035babde9a37b04df90dcde1a81349c0
Unknown <james.smith@heig-vd.ch>
2017-10-12 11:27:29 +0200

    Implémentation du constructeur
    Create of constructors, toString method

--- Labo5_Matrix/src/labo5_matrix/Matrix.java
+++ Labo5_Matrix/src/labo5_matrix/Matrix.java
@@ -10,5 +10,50 @@ package labo5_matrix;
  * @author James
  */
 public class Matrix {
+   private boolean[][] matrix;
    
+   Matrix(int n, boolean ... args){
+      // Check du bon nombre d'argument
+      if(args.length != n*n){
+         throw new RuntimeException("Wrong nomber of arguments");
+      }
+      
+      // Initialisation de la matrice
+      int i = 0, j = 0;
+      matrix = new boolean[n][n];
+      // Remplissage de la matrice
+      for(boolean b: args){
+         matrix[i][j] = b;
+         if(++i >= n){
+            ++j;
+            i = 0;
+         }
+      }
+   }
+   
+   Matrix(int n){
+      // Initialisation de la matrice avec la valeur par défaut 0
+      matrix = new boolean[n][n];
+      // Remplissage de la matrice
+      for(boolean[] line : matrix){
+         for(boolean cell : line){
+            cell = Math.random() >= 0.5;
+         }
+      }
+   }
+   
+   Matrix(){
+      
+   }
+   
+   public String toString(){
+      String print = new String();
+      for(boolean[] line : matrix){
+         for(boolean cell : line){
+            print += cell + " ";
+         }
+         print += "\n";
+      }
+      return print;
+   }
 }
